6fde0c76e7862b7141f228162135aab4a73e9fce
Mark Franciose <mark.franciose@gmail.com>
2017-11-29 22:24:52 -0600

    change welcome page to Victor Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+				<title>Victor (the) Page</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,9 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Victor</div>
+								<div><h2>(the)</h2></div>
+								<div class="title">Page</div>
             </div>
         </div>
     </body>
